292b27234a9a16082e3c66a2f9df55d7dab8c14c
Architect <greatraksin@icloud.com>
2020-05-09 00:58:44 +0300

    Show first and last points of random walk

--- rwMultVis.py
+++ rwMultVis.py
@@ -10,6 +10,11 @@ while True:
 
     plt.title('Случайное блуждание', fontsize=24)
     plt.scatter(rw.x_val, rw.y_val, c=pNumber, cmap=plt.cm.plasma, s=5)
+
+    # выделяем первую и последние точки
+    plt.scatter(0, 0, c='green', s=100)
+    plt.scatter(rw.x_val[-1], rw.y_val[-1], c='red', s=100)
+
     plt.show()
 
     run = input('Сгенерировать еще одно блуждание? ')
--- rwVisualizing.py
+++ rwVisualizing.py
@@ -9,4 +9,8 @@ pNumber = list(range(rw.numberOfPoints))
 
 plt.title('Случайное блуждание', fontsize=24)
 plt.scatter(rw.x_val, rw.y_val, c=pNumber, cmap=plt.cm.plasma, s=5)
+
+# выделяем первую и последние точки
+plt.scatter(0, 0, c='green', s=100)
+plt.scatter(rw.x_val[-1], rw.y_val[-1], c='red', s=100)
 plt.show()
