697f6ace79e7b21ff66418a72d1f147653881bbe
Architect <greatraksin@icloud.com>
2020-05-09 00:54:43 +0300

    Adding multivisualizing and correct color map

---
+++ rwMultVis.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+from rw import RandomWalk
+
+# строим случайное блуждание и наносим точки на диаграмму
+while True:
+    rw = RandomWalk()
+    rw.fillWalk()
+
+    pNumber = list(range(rw.numberOfPoints))
+
+    plt.title('Случайное блуждание', fontsize=24)
+    plt.scatter(rw.x_val, rw.y_val, c=pNumber, cmap=plt.cm.plasma, s=5)
+    plt.show()
+
+    run = input('Сгенерировать еще одно блуждание? ')
+    if run.lower() == 'n':
+        break
--- rwVisualizing.py
+++ rwVisualizing.py
@@ -5,7 +5,8 @@ from rw import RandomWalk
 rw = RandomWalk()
 rw.fillWalk()
 
+pNumber = list(range(rw.numberOfPoints))
 
 plt.title('Случайное блуждание', fontsize=24)
-plt.scatter(rw.x_val, rw.y_val, c=rw.x_val, cmap=plt.cm.inferno, s=5)
+plt.scatter(rw.x_val, rw.y_val, c=pNumber, cmap=plt.cm.plasma, s=5)
 plt.show()
